ad3c86afe2de9e5c5186fa7f9bdd2f96817dc962
Ingrid Rodriguez <sergio_9180@hotmail.com>
2024-08-16 23:19:47 -0600

    Se actualiza el archivo app

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import pandas as pd
 import plotly.express as px
 import streamlit
+
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+st.header('Venta de coches')
+
+hist_button = st.button('Construir histograma') # crear un botón
+
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+# crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+        
