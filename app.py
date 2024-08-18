abc846297bc4e787094967a03996a211af201327
Ingrid Rodriguez <sergio_9180@hotmail.com>
2024-08-18 13:18:07 -0600

    Completar codigo app.py

--- app.py
+++ app.py
@@ -18,4 +18,13 @@ if hist_button:
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
-        
+
+dist_button = st.button('Construir un diagrama de dispersión') # crear un botón
+
+if dist_button:
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    fig2 = px.scatter(car_data, x="odometer")
+    
+    st.plotly_chart(fig2, use_container_width=True)
+    
