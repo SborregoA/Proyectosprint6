7906de35af10396ca3aab94faf20749b69b4a294
Ingrid Rodriguez <sergio_9180@hotmail.com>
2024-08-18 20:44:05 -0600

    modificacion grafica dispersión  codigo app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.header('Venta de coches')
 
 print(car_data.head(15))
 
-build_histogram = st.checkbox('Construir histograma') # crear un botón
+build_histogram = st.checkbox('Construir histograma') # casilla de selección
 
 if build_histogram:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -19,9 +19,9 @@ if build_histogram:
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-dist_button = st.button('Construir un diagrama de dispersión') # crear un botón
+build_disp = st.checkbox('Construir un diagrama de dispersión') # casilla de selección
 
-if dist_button:
+if build_disp:
     st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
     
     fig2 = px.scatter(car_data, y="odometer")
